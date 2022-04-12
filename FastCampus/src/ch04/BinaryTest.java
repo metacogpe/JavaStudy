6f73303e6e4f268555f54308a9acb6bc9fa30f34
metacog <metacog@kakao.com>
2022-04-13 08:39:06 +0900

    binary number

---
+++ FastCampus/src/ch04/BinaryTest.java
@@ -0,0 +1,14 @@
+package ch04;
+
+public class BinaryTest {
+    public static void main(String[] args) {
+        int num = 10;
+        int bNum = 0B1010;
+        int oNum = 012;
+        int xNum = 0XA;
+        System.out.println("num = " + xNum);
+        System.out.println("bNum = " + xNum);
+        System.out.println("oNum = " + xNum);
+        System.out.println("xNum = " + xNum);
+    }
+}
